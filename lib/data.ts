1c84ad0cde9c5b4c5ff269f65c7222e29f2030da
Otis Alejandro <128675915+OtisAlejandro@users.noreply.github.com>
2023-10-10 20:44:36 -0500

    Skills

--- lib/data.ts
+++ lib/data.ts
@@ -53,3 +53,25 @@ export const projectsData = [
     imageUrl: sushimeister,
   },
 ] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "Prisma",
+  "MongoDB",
+  "Redux",
+  "GraphQL",
+  "Apollo",
+  "Express",
+  "PostgreSQL",
+  "Python",
+  "Django",
+  "Framer Motion",
+] as const;
