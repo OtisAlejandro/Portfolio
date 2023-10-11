aecf59c02c08b0d072cee51580cf587939d8cad7
Otis Alejandro <128675915+OtisAlejandro@users.noreply.github.com>
2023-10-10 23:35:03 -0500

    footer

---
+++ components/footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="mb-5 px-4 text-center text-gray-500">
+      <small className="mb-2 block text-xs">&copy; 2023 Otis. All Rights Reserved.</small>
+    </footer>
+  );
+}
